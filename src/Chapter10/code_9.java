110920082ba54e71b3660235577e6605ebe4cb06
Citrix <sijieqian@foxmail.com>
2018-08-14 21:40:07 +0800

    Chapter10

---
+++ src/Chapter10/code_9.java
@@ -0,0 +1,14 @@
+package Chapter10;
+
+/**
+ * Created by Citrix on 2018/8/14.
+ */
+public class code_9 {
+    public static void main(String[] args) {
+        if (true) {
+            System.out.println("block 1");
+        } else {
+            System.out.println("block 2");
+        }
+    }
+}
